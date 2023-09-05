ff2656bfbe0ccf8b596d51c4e485c59ad0ce29de
ffespechen <flavio.espechenieva@gmail.com>
2023-09-05 20:59:38 -0300

    ADD: Ejercicio 001 Hello world!

---
+++ ex001_helloworld/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, world!");
+Console.WriteLine("What is your name?");
+
+string? nombre = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(nombre))
+{
+    nombre = "DESCONOCIDO MISTERIOSO";
+}
+
+Console.WriteLine($"Hello, {nombre}");
