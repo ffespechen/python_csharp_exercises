3646dc2af3d430b13a6d1a5243bb33632d78d0f6
ffespechen <flavio.espechenieva@gmail.com>
2023-09-05 22:14:14 -0300

    ADD: Ejercicio 004 Area y Volumen

--- ex004_area_and_volume/Program.cs
+++ ex004_area_and_volume/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rnd = new Random();
+int largo = rnd.Next(1, 20);
+int ancho = rnd.Next(1, 20);
+int altura = rnd.Next(1,30);
+
+Console.WriteLine("---- Parámetros ----");
+Console.WriteLine($"Largo:  {largo, 5:N0}");
+Console.WriteLine($"Ancho:  {ancho, 5:N0}");
+Console.WriteLine($"Altura: {altura, 5:N0}");
+
+Console.WriteLine("---- Resultados ----");
+Console.WriteLine($"Área:       {Area(largo, ancho), 5:N0}");
+Console.WriteLine($"Perímetro:  {Perimeter(largo, ancho), 5:N0}");
+Console.WriteLine($"Volumen:    {Volume(largo, ancho, altura), 5:N0}");
+Console.WriteLine($"Superfice:  {SurfaceArea(largo, ancho, altura), 5:N0}");
