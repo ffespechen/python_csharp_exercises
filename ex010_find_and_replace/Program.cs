123fcf240c37229f67def03051912599ca62d7bf
fenieva@santacruz.gob.ar <fenieva@santacruz.gob.ar>
2023-09-06 11:16:49 -0300

    REFACTOR: Ejercicio 010 Ingreso de textos por usuario y validación de no nulos

--- ex010_find_and_replace/Program.cs
+++ ex010_find_and_replace/Program.cs
@@ -1,2 +1,22 @@
-﻿// Encontrar y reemplazar
-Console.WriteLine(FindAndReplace("ABCDEFGHIJK", "AB", "xxx"));
+﻿Console.Write("Texto de trabajo >> ");
+string? texto = Console.ReadLine();
+Console.Write("Texto a buscar >> ");
+string? textoViejo = Console.ReadLine();
+Console.Write("Texto de reemplazo >> ");
+string? textoNuevo = Console.ReadLine();
+
+if(String.IsNullOrWhiteSpace(texto))
+{
+    texto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+if (String.IsNullOrWhiteSpace(textoViejo))
+{
+    textoViejo = "EF";
+} 
+if(String.IsNullOrWhiteSpace(textoNuevo))
+{
+    textoNuevo = "xxx";
+}
+
+// Encontrar y reemplazar
+Console.WriteLine(FindAndReplace(texto, textoViejo, textoNuevo));
