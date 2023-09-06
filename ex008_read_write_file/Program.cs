f54f9a789f82bfa394fbd387134f4982f216435d
ffespechen <flavio.espechenieva@gmail.com>
2023-09-05 23:51:44 -0300

    ADD: Ejercicio 008 Leer y Escribir en archivos (versión sin funciones)

---
+++ ex008_read_write_file/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Nombre del archivo de destino >> ");
+string? archivo = Console.ReadLine();
+Console.Write("Texto a guardar en el archivo >> ");
+string? texto = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(archivo))
+{
+    archivo = "archivo.txt";
+}
+
+if (string.IsNullOrWhiteSpace(texto))
+{
+    texto = "Lorem ipsum...";
+}
+
+string ruta = Path.Combine(Environment.CurrentDirectory, archivo);
+
+// Versión sin funciones
+Console.WriteLine("Guardando texto...");
+using (StreamWriter sw = File.CreateText(ruta))
+{
+    sw.WriteLine(texto);
+}
+
+Console.Write("Ingrese texto a agregar >> ");
+texto = Console.ReadLine() ?? "Texto a agregar";
+Console.WriteLine("--- Agregando texto ---");
+using (StreamWriter sw = File.AppendText(ruta))
+{
+    sw.WriteLine(texto);
+}
+
+Console.WriteLine("--- Mostrando texto guardado ---");
+foreach (string linea in File.ReadLines(ruta))
+{
+    Console.WriteLine(linea);
+}
