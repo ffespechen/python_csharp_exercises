f32589c513d37e95395436c5ea9076779743d93d
ffespechen <flavio.espechenieva@gmail.com>
2023-09-05 21:46:22 -0300

    ADD: Ejercicio 003 ODD & EVEN

---
+++ ex003_odd_even/Program.Helpers.cs
@@ -0,0 +1,12 @@
+partial class Program
+{
+    public static bool IsOdd(int number)
+    {
+        return number % 2 != 0;
+    }
+
+    public static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+}
---
+++ ex003_odd_even/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+const int ITERACIONES = 12;
+
+for (int i = 0; i < ITERACIONES; i++ )
+{
+    int numero = rnd.Next();
+    Console.WriteLine($"IsEven() aplicada a {numero,20:N0} resulta {IsEven(numero)}");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < ITERACIONES; i++ )
+{
+    int numero = rnd.Next();
+    Console.WriteLine($"IsOdd() aplicada a {numero,20:N0} resulta {IsOdd(numero)}");
+}
