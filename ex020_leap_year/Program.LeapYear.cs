9891139c20768816c516ccf828f4ee98dff0957e
ffespechen <flavio.espechenieva@gmail.com>
2023-09-09 12:38:39 -0300

    ADD: Ejercicio 20 Leap Year (Año Bisiesto)

---
+++ ex020_leap_year/Program.LeapYear.cs
@@ -0,0 +1,22 @@
+partial class Program
+{
+    public static bool IsLeapYear(int year)
+    {
+        if (year % 400 == 0)
+        {
+            return true;
+        }
+        else if (year % 100 == 0)
+        {
+            return false;
+        }
+        else if (year % 4 == 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+}
---
+++ ex020_leap_year/Program.cs
@@ -0,0 +1,8 @@
+﻿// https://inventwithpython.com/pythongently/exercise20/
+
+int[] years = {1999, 2000, 2001, 2004, 2100, 1900, 2400, 1976};
+
+for (int i = 0; i < years.Length; i++)
+{
+    Console.WriteLine($"Año {years[i]} es Bisiesto? {IsLeapYear(years[i])}");
+}
